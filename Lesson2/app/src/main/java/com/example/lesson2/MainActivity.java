b42c18f486b2595883b9128755c23c744afe02ba
Roman-Kov <rojer_ko@bk.ru>
2019-12-02 01:12:05 +0300

    Исправил структуру аппа

--- Lesson2/app/src/main/java/com/example/lesson2/MainActivity.java
+++ Lesson2/app/src/main/java/com/example/lesson2/MainActivity.java
@@ -16,35 +16,45 @@ import java.util.HashMap;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+//123
+    private ImageView cityEmblemView;
+    private TextView cityTemperatureView;
+    private Spinner citySpinner;
+    private TextView dateTextView;
+    private Emblems cityEmblems = new Emblems();
+    private Temperatures cityTemperatures = new Temperatures();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        onSelectSpinnersItem();
+    }
 
-        final ImageView cityEmblemView = findViewById(R.id.cityEmblem);
-        final TextView cityTemperatureView = findViewById(R.id.cityTemperature);
-        final Spinner citySpinner = findViewById(R.id.citySpinner);
-        final TextView dateTextView = findViewById(R.id.date);
-        final Emblems cityEmblems = new Emblems();
-        final Temperatures cityTemperatures = new Temperatures();
+    void initViews(){
+        cityEmblemView = findViewById(R.id.cityEmblem);
+        cityTemperatureView = findViewById(R.id.cityTemperature);
+        citySpinner = findViewById(R.id.citySpinner);
+        dateTextView = findViewById(R.id.date);
+        cityEmblems = new Emblems();
+        cityTemperatures = new Temperatures();
+    }
 
+    void onSelectSpinnersItem(){
         AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
                 String selectedCity = parent.getSelectedItem().toString();
                 cityEmblems.setEmblemByCity(cityEmblemView, selectedCity);
                 cityTemperatures.setTemperatureByCity(cityTemperatureView, selectedCity);
-                String today = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+                String today = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(new Date());
                 dateTextView.setText(today);
-
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         };
         citySpinner.setOnItemSelectedListener(itemSelectedListener);
