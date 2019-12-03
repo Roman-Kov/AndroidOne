80abf0262f5c4dd6d84bc0e56af49c8b32d99720
Roman-Kov <rojer_ko@bk.ru>
2019-12-04 01:11:29 +0300

    Добавил второй активити, хранение данных организовал в синглтоне, на втором активити добавил кнопки для просмотра Яндекс погоды и Wiki

--- Lesson2/app/src/main/java/com/example/lesson2/MainActivity.java
+++ Lesson2/app/src/main/java/com/example/lesson2/MainActivity.java
@@ -2,27 +2,27 @@ package com.example.lesson2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
-//123
+public class MainActivity extends AppCompatActivity implements Constants{
+
     private ImageView cityEmblemView;
-    private TextView cityTemperatureView;
     private Spinner citySpinner;
-    private TextView dateTextView;
-    private Emblems cityEmblems = new Emblems();
-    private Temperatures cityTemperatures = new Temperatures();
+    private Button showWeatherButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +30,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
         onSelectSpinnersItem();
+        onShowWeatherBtnClick();
     }
 
     void initViews(){
         cityEmblemView = findViewById(R.id.cityEmblem);
-        cityTemperatureView = findViewById(R.id.cityTemperature);
         citySpinner = findViewById(R.id.citySpinner);
-        dateTextView = findViewById(R.id.date);
-        cityEmblems = new Emblems();
-        cityTemperatures = new Temperatures();
+        showWeatherButton = findViewById(R.id.showWeatherButton);
     }
 
     void onSelectSpinnersItem(){
@@ -47,10 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedCity = parent.getSelectedItem().toString();
-                cityEmblems.setEmblemByCity(cityEmblemView, selectedCity);
-                cityTemperatures.setTemperatureByCity(cityTemperatureView, selectedCity);
-                String today = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(new Date());
-                dateTextView.setText(today);
+                cityEmblemView.setImageResource(CityValues.getInstance().getEmblemByCity(selectedCity));
             }
 
             @Override
@@ -58,5 +53,17 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         citySpinner.setOnItemSelectedListener(itemSelectedListener);
+    }
+
+    void onShowWeatherBtnClick(){
+        showWeatherButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                intent.putExtra(CITY_NAME, citySpinner.getSelectedItem().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
